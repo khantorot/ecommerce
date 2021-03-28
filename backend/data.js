b5bdf4b1928deef06b94a43145caa5aa16f1323c
iamjustkhan <khantoro110811@gmail.com>
2021-03-28 22:50:49 +0600

    init

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Revolt Pro1',
+            category: 'sport',
+            image: '/images/p.jpg',
+            price: 111,
+            countInStock: 4,
+            brand: 'Tesla',
+            rating: 1.5,
+            numReviews: 163,
+            description: 'some text here'
+        },
+        {
+            _id: '2',
+            name: 'Revolt Pro2',
+            category: 'sport',
+            image: '/images/p1.jpg',
+            price: 222,
+            countInStock: 0,
+            brand: 'Tesla',
+            rating: 5,
+            numReviews: 63,
+            description: 'some text here1'
+        },
+        {
+            _id: '3',
+            name: 'Revolt Pro3',
+            category: 'sport',
+            image: '/images/p.jpg',
+            price: 333,
+            countInStock: 2,
+            brand: 'Tesla',
+            rating: 4.5,
+            numReviews: 63,
+            description: 'some text here'
+        },
+        {
+            _id: '4',
+            name: 'Revolt Pro4',
+            category: 'sport',
+            image: '/images/p1.jpg',
+            price: 444,
+            countInStock: 14,
+            brand: 'Tesla',
+            rating: 3,
+            numReviews: 23,
+            description: 'some text here1'
+        },
+    ]
+}
+
+export default data;
---
+++ frontend/src/data.js
@@ -0,0 +1,30 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Revolt Pro',
+            category: 'sport',
+            image: '/images/p.jpg',
+            price: 111,
+            countInStock: 4,
+            brand: 'Tesla',
+            rating: 1.5,
+            numReviews: 163,
+            description: 'some text here'
+        },
+        {
+            _id: '2',
+            name: 'Revolt Pro1',
+            category: 'sport',
+            image: '/images/p1.jpg',
+            price: 222,
+            countInStock: 0,
+            brand: 'Tesla',
+            rating: 5,
+            numReviews: 63,
+            description: 'some text here1'
+        },
+    ]
+}
+
+export default data;
